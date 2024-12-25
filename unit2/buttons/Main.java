7fa95bc0141ce968f45217fffd3e38b0a44149ae
Naman Karki <nomanakarki56@gmail.com>
2024-12-25 11:05:22 +0545

    unit2

---
+++ unit2/buttons/Main.java
@@ -0,0 +1,8 @@
+package buttons;
+
+public class Main {
+    public static void main(String[] args) {
+        MyFrame frame = new MyFrame();
+    }
+
+}
--- unit2/panels/Main.java
+++ unit2/panels/Main.java
@@ -2,15 +2,31 @@ package panels;
 
 import java.awt.Color;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Main {
 public static void main(String[] args) {
 
-    JPanel redpanel = new JPanel();
+
+    ImageIcon icon = new ImageIcon("panels/test.png");
+    JLabel label = new JLabel();
+    label.setText("hi");
+    label.setIcon(icon);
+
+    JPanel redpanel = new JPanel();// a gui component that functions as a container to hold other components
     redpanel.setBackground(Color.red);
     redpanel.setBounds(0,0,250,250);
+
+    JPanel bluepanel = new JPanel();
+    bluepanel.setBackground(Color.blue);
+    bluepanel.setBounds(250,0,250,250);
+
+    JPanel greenpanel = new JPanel();
+    greenpanel.setBackground(Color.green);
+    greenpanel.setBounds(500,0,250,250);
 
 
 
@@ -20,6 +36,9 @@ public static void main(String[] args) {
     frame.setLayout(null);
     frame.setSize(750,750);
     frame.setVisible(true);
+    greenpanel.add(label);
     frame.add(redpanel);
+    frame.add(bluepanel);
+    frame.add(greenpanel);
 }
 }
